396ab3f269131b876005e565ec3d7d3d04f9cf18
Mikko Loponen <mikko.loponen@iki.fi>
2018-02-07 16:35:05 +0200

    4.5-4.6 Favorite and most blogs

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -12,6 +12,37 @@ describe('total likes', () => {
 
 })
 
+describe('favorite blog', () => {
+
+    test('of empty list is undefined', () => {
+        expect(listHelper.favoriteBlog([])).toBe(undefined)
+    })
+
+    test('of many blogs is correct', () => {
+        expect(listHelper.favoriteBlog(blogs)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            title: 'Canonical string reduction',
+            likes: 12
+        })
+    })
+
+})
+
+describe('most blogs', () => {
+
+    test('of empty list is undefined', () => {
+        expect(listHelper.mostBlogs([])).toBe(undefined)
+    })
+
+    test('of many blogs Robert C. Martin with 3 blogs', () => {
+        expect(listHelper.mostBlogs(blogs)).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+
+})
+
 const blogs = [
     {
         _id: '5a422a851b54a676234d17f7',
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,53 @@ const totalLikes = (blogs) => {
     }
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+
+    let mostLikes = -1
+    let favoriteObject
+
+    for (let index = 0; index < blogs.length; index++) {
+        if (blogs[index].likes > mostLikes) {
+            mostLikes = blogs[index].likes
+            favoriteObject = blogs[index]
+        }
+    }
+    return {
+        title: favoriteObject.title,
+        author: favoriteObject.author,
+        likes: favoriteObject.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    let myMap = new Map()
+    let mostBlogs = 0
+
+    // Create a map of authors and their blog counts while finding the maximum number of blogs
+    blogs.forEach(blog => {
+        if (myMap.has(blog.author)){
+            let numberOfBlogs = myMap.get(blog.author)+1
+            myMap.set(blog.author, numberOfBlogs)
+            mostBlogs = Math.max(mostBlogs, numberOfBlogs)
+        } else {
+            myMap.set(blog.author, 1)
+            mostBlogs = Math.max(mostBlogs, 1)
+        }
+    })
+
+    // Return the first occurrence of highest number of blogs
+    for(let [key, value] of myMap){
+        if (value === mostBlogs){
+            return({'author': key, 'blogs': value})
+        }
+    }
+}
+
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
