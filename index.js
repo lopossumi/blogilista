064595ce4fc36331d400f423c8ba07a12368d7f4
Mikko Loponen <mikko.loponen@iki.fi>
2018-02-10 13:40:24 +0200

    4.18 login

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 
@@ -15,8 +17,11 @@ mongoose.Promise = global.Promise
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.logger)
+
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
+
 app.use(middleware.error)
 
 const server = http.createServer(app)
